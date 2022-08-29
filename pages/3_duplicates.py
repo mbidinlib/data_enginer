8263a8f82961cc791e85d9b193cb86c1836369fd
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-08-29 22:23:02 +0000

    Add files via upload

--- pages/3_duplicates.py
+++ pages/3_duplicates.py
@@ -21,10 +21,16 @@ st.markdown("# Check Duplicates ")
 st.sidebar.markdown("# Check Duplicates-")
 
 #df = importdata.load_page()
-df1_name = st.session_state['df1']
+if st.session_state['df1'] is not None:
+     try:
+          data1 = pd.read_csv(st.session_state['df1'])
+     except Exception as e:
+          print(e)
+          data1 = pd.read_excel(st.session_state['df1'])
+     st.write(data1)
 
 option = st.selectbox(
      'Select the dataset you want to check',
-     (df1_name, "data2"))
+     (data1, "data2"))
 st.write(option)
 st.write(st.session_state['df1'])
