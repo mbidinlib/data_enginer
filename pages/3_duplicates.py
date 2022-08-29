5f7062b5f5c2e1a3e8cc85145866ed85d075fa9f
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-08-29 22:10:06 +0000

    Add files via upload

---
+++ pages/3_duplicates.py
@@ -0,0 +1,27 @@
+
+'''
+Date: Wed Aug 23 21:12:18 2022
+@author: Glooory
+Purpose: Data Engineering
+'''
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+#Import pages
+#from streamlit_multipage import MultiPage
+
+
+st.markdown("# Check Duplicates ")
+st.sidebar.markdown("# Check Duplicates-")
+
+#df = importdata.load_page()
+option = st.selectbox(
+     'Select the dataset you want to check',
+     ("data1", "data2"))
+st.write(option)
