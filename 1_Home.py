1dc44d5072a0e00b2a6d64ebc59f7a8b760a081c
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-01 11:20:42 +0000

    Add files via upload

--- 1_Home.py
+++ 1_Home.py
@@ -11,11 +11,10 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-import time as tm
+from persist import persist, load_widget_state
 
 #Installed programs
 #pip install streamlit-multipage
-
 
 #Add background color
 st.markdown(
@@ -81,89 +80,22 @@ def header(st):
     st.write("This app is free to use")
 
 
+if "page" not in st.session_state:
+    # Initialize session state.
+    st.session_state.update({
+        # Default page.
+        "page": "Home",
+    })
 
+page = st.sidebar.radio("Select your page", tuple(PAGES.keys()), format_func=str.capitalize)
 
-#********************
-# Manage Session State
-# *********************
-import streamlit.ReportThread as ReportThread
-from streamlit.server.Server import Server
+PAGES[page]()
 
-def get_session():
-    ctx = ReportThread.get_report_ctx().session_id
-    this_session = None
-    current_session = Server.get_current()._get_session_info(ctx)
-    this_session = current_session.session
-    # st.write("ch",current_session.session)
+PAGES = {
+    "Home": Home,
+    "Import": import,
+}
 
-    if current_session is None:
-        raise RuntimeError(
-            "Oh noes. Couldn't get your Streamlit Session object"
-            'Are you doing something fancy with threads?')
-
-    return this_session
-
-def get_state(hash_funcs=None):
-    session = get_session()
-    if not hasattr(session, "_custom_session_state"):
-        session._custom_session_state = SessionState(session, hash_funcs)
-
-    return session._custom_session_state
-
-class SessionState:
-
-    def __init__(self, session, hash_funcs):
-        """Initialize SessionState instance."""
-        self.__dict__["_state"] = {
-            "data": {},
-            "hash": None,
-            "is_rerun": False,
-            "session": session,
-        }
-
-    def __call__(self, **kwargs):
-        """Initialize state data once."""
-        for item, value in kwargs.items():
-            if item not in self._state["data"]:
-                self._state["data"][item] = value
-
-    def __getitem__(self, item):
-        """Return a saved state value, None if item is undefined."""
-        return self._state["data"].get(item, None)
-
-    def __getattr__(self, item):
-        """Return a saved state value, None if item is undefined."""
-        return self._state["data"].get(item, None)
-
-    def __setitem__(self, item, value):
-        """Set state value."""
-        self._state["data"][item] = value
-
-    def __setattr__(self, item, value):
-        """Set state value."""
-        self._state["data"][item] = value
-
-    def clear(self):
-        """Clear session state and request a rerun."""
-        self._state["data"].clear()
-        self._state["session"].request_rerun()
-
-    def sync(self):
-        """Rerun the app with all state values up to date from the beginning to fix rollbacks."""
-
-        # Ensure to rerun only once to avoid infinite loops
-        # caused by a constantly changing state value at each run.
-        #
-        # Example: state.value += 1
-        if self._state["is_rerun"]:
-            self._state["is_rerun"] = False
-
-        elif self._state["hash"] is not None:
-            if self._state["hash"] != self._state["hasher"].to_bytes(self._state["data"], None):
-                self._state["is_rerun"] = True
-                self._state["session"].request_rerun()
-
-        self._state["hash"] = self._state["hasher"].to_bytes(self._state["data"], None)
-
-
- 
+if __name__ == "__main__":
+    load_widget_state()
+    main()
