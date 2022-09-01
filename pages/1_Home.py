a43fabc79a9db55fc0b909c400c1c6b8d0f72ccd
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-01 15:46:24 +0000

    Add files via upload

---
+++ pages/1_Home.py
@@ -0,0 +1,81 @@
+
+'''
+Date: Wed Aug 23 21:12:18 2022
+@author: Glooory
+Purpose: Data Engineering
+'''
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from persist import persist, load_widget_state
+
+#Installed programs
+#pip install streamlit-multipage
+
+#Add background color
+st.markdown(
+    '''<style>
+    body {
+        background-color: brown;
+        secondary-background-color:red;
+        }
+    h1 {
+        color: maroon;
+        font-family: Tahoma;
+        font-size: 200%;
+    }
+    h2 {
+        color: #BA4A00;
+        font-family: open sans;
+    }
+    h3 {
+        color: #DC7633;
+        font-family: oopen sans;
+    }
+    p {
+        font-family: open sans;
+    }    
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+</style>
+<body style="background-color:#1c87c9;">
+</body>
+
+''', unsafe_allow_html=True)
+
+
+#st.sidebar.markdown("# Home page")
+with st.sidebar.container():
+    st.image('picture.jpg',
+    caption='Mathew Bidinlib', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    #st.image(image, width= use_column_width=True)
+
+st.markdown("# MGlory Data Engineering Toolkit ")
+st.markdown("""
+    Welcome to MGlory Data Engineering toolkit.
+    This is a python based tool that would help you prepare your dataset for analysis.
+    The tool contains the following components:
+    - **Data Import**
+    With the data import page, you would be able to import upto five (5) datasets for your work.
+    Each dataset is in a seperate tab and you can navigate across tabs
+    - **Check Duplicates**
+    You can check if your data has duplicates using some key variables 
+    and also remove duplicates if there are any
+    - **Check Outliers**
+    You can check if some variables have outliers given a certain multiplier effect
+    - **Merge Data**
+    This would help you to merge selected datasets that you imported into one. 
+    You may also append datasets if needed
+    """)
+
+def footer(st):
+    st.write("Developed by [Mathew Bidinlib](https://github.com/mbidinlib/)")
+
+
+def header(st):
+    st.write("This app is free to use")
+
