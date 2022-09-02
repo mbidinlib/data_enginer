8a28274a94badc6695c6200b6d7be087232dc6e9
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-02 12:03:52 +0000

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 # Set page outline
 st. set_page_config(layout="wide")
-
+st.set_page_config(page_title="MGlory Data-Engineering", page_icon="📊")
 #Installed programs
 #pip install streamlit-multipage
 
