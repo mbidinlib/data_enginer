276c99ccdd2f7122b92558ca0c7c7b8e42348c56
Mathew Bidinlib <61548432+mbidinlib@users.noreply.github.com>
2022-09-10 00:24:33 -0500

    fixed resolve duplicates bug

--- pages/3_Duplicates.py
+++ pages/3_Duplicates.py
@@ -131,11 +131,11 @@ if ("dataset1" in st.session_state or "dataset2" in st.session_state or "dataset
                     if keepfirst or keeplast or keepnone:
                          dupdrop_name = st.text_input("Optional:Give your corrected dataset a shortname. Default if dupdrop_data", key="dupdrop_name")
                          if dupdrop_name:
-                              st.session_state["dupdrop_name"] = dupdrop_data
-                              st.session_state["dupdrop_name1"] = dupdrop_name
+                              st.session_state["dupdrop_name1"] = dupdrop_data
+                              st.session_state["dupdrop_name2"] = dupdrop_name
                          else:
-                             st.session_state["dupdrop_name"] = dupdrop_data 
-                         if 'dupdrop_name' in st.session_state:
+                             st.session_state["dupdrop_name1"] = dupdrop_data 
+                         if 'dupdrop_name1' in st.session_state:
                               st.markdown(st.session_state["dupdrop_name1"])   
                          dup_drop_down = dupdrop_data.to_csv().encode('utf-8')
                          st.download_button(label = 'Export duplicates', data = dup_drop_down, 
